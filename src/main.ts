54f61d2dc737cd56adebeeb641b423bd667a0afc
unknown <arthursantos.s@hotmail.com>
2024-10-26 18:14:33 -0300

    feat(docs): integrate Swagger for API documentation and enable CORS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+import { AppModule } from './app/app.module';
+
+import { envs } from '@app/core/configuration/envs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  const config = new DocumentBuilder()
+    .setTitle('Arrow Digital Desafio API Api docs')
+    .setDescription('Aqui se encontra toda a documentação da api do Desafio Arrow Digital.')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(Number(envs.PORT));
 }
-bootstrap();
+
+void bootstrap();
