10b53c50529c56e586fa8c2d1f02c7fe41435b82
unknown <arthursantos.s@hotmail.com>
2024-10-27 12:05:48 -0300

    refactor(main): config scarla setup

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,31 @@
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
 import { envs } from '@app/core/configuration/envs';
+import { apiReference } from '@scalar/nestjs-api-reference';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
   const config = new DocumentBuilder()
-    .setTitle('Arrow Digital Desafio API Api docs')
-    .setDescription('Aqui se encontra toda a documentação da api do Desafio Arrow Digital.')
+    .setTitle('Arrow Digital Desafio API')
+    .setDescription('Aqui se encontra toda a documentação da API do Desafio Arrow Digital.')
     .setVersion('1.0')
     .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+
+  app.use(
+    '/docs',
+    apiReference({
+      spec: {
+        content: document,
+      },
+    })
+  );
 
   await app.listen(Number(envs.PORT));
 }
